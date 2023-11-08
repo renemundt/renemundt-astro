1dfca0b9d0f58b17cc46715a2cbe8d419e1af544
René Mundt <renem@live.dk>
2023-11-08 17:40:09 +0100

    add welcome banner

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://renemundt.netlify.app/"
+  site: "https://renemundt.netlify.app/",
+  integrations: [preact()]
 });

a455614fccb46214e92a7f959cbbe0937bb29845
René Mundt <renem@live.dk>
2023-11-28 16:16:00 +0100

    nav full length

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,4 +1,4 @@
-import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import rss, { pagesGlobToRssItems } from '@astrojs/rss'
 
 export async function GET(context) {
   return rss({
@@ -7,5 +7,5 @@ export async function GET(context) {
     site: context.site,
     items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
     customData: `<language>en-us</language>`,
-  });
+  })
 }

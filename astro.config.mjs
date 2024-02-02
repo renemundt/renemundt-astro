b41940324fa8552642ffedff2791e04bc9608486
René Mundt <renem@live.dk>
2024-02-02 15:08:20 +0100

    add padding and remove shiki config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,26 +6,4 @@ import preact from '@astrojs/preact'
 export default defineConfig({
   site: 'https://renemundt.netlify.app/',
   integrations: [preact()],
-  markdown: {
-    shikiConfig: {
-      // Choose from Shiki's built-in themes (or add your own)
-      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
-      theme: 'dracula',
-      // Alternatively, provide multiple themes
-      // https://shikiji.netlify.app/guide/dual-themes#light-dark-dual-themes
-      experimentalThemes: {
-        light: 'github-light',
-        dark: 'github-dark',
-      },
-      // Add custom languages
-      // Note: Shiki has countless langs built-in, including .astro!
-      // https://github.com/shikijs/shiki/blob/main/docs/languages.md
-      langs: ['javascript'],
-      // Enable word wrap to prevent horizontal scrolling
-      wrap: true,
-      // Add custom transformers: https://shikiji.netlify.app/guide/transformers
-      // Find common transformers: https://shikiji.netlify.app/packages/transformers
-      transformers: [],
-    },
-  },
 })
